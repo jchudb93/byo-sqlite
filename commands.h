8b3b7a4ef6b31d104e3c08b32820a6c7ae051a60
Jose Chu <jchudb93@gmail.com>
2019-10-20 17:50:24 -0500

    offsets

--- commands.h
+++ commands.h
@@ -7,7 +7,6 @@
 #ifndef COMMANDS
 #define COMMANDS
 
-
 typedef enum
 {
     META_COMMAND_SUCCESS,
@@ -25,7 +24,8 @@ typedef enum
     PREPARE_UNRECOGNIZED_STATEMENT
 } PrepareResult;
 
-typedef struct {
+typedef struct
+{
     uint32_t id;
     char username[COLUMN_USERNAME_SIZE];
     char email[COLUMN_EMAIL_SIZE];
@@ -36,7 +36,7 @@ typedef struct
     Row row_to_insert;
 } Statement;
 
-#define size_of_attribute(Struct, Attribute) sizeof(((Struct*)0)->Attribute)
+#define size_of_attribute(Struct, Attribute) sizeof(((Struct *)0)->Attribute)
 const uint32_t ID_SIZE = size_of_attribute(Row, id);
 const uint32_t USERNAME_SIZE = size_of_attribute(Row, username);
 const uint32_t EMAIL_SIZE = size_of_attribute(Row, email);
@@ -45,21 +45,41 @@ const uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
 const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
 const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
 
-void serialize_row(Row* source, void* destination) {
+void serialize_row(Row *source, void *destination)
+{
     memcpy(destination + ID_OFFSET, &(source->id), ID_SIZE);
     memcpy(destination + USERNAME_OFFSET, &(source->username), USERNAME_SIZE);
     memcpy(destination + EMAIL_OFFSET, &(source->email), EMAIL_SIZE);
+}
+
+void deserialize_row(void *source, Row *destination)
+{
+    memcpy(&(destination->id), source + ID_OFFSET, ID_SIZE);
+    memcpy(&(destination->username), source + USERNAME_OFFSET, USERNAME_SIZE);
+    memcpy(&(destination->email), source + EMAIL_OFFSET, EMAIL_SIZE);
 }
 
 const uint32_t PAGE_SIZE = 4096;
 const uint32_t ROWS_PER_PAGE = PAGE_SIZE / ROW_SIZE;
 const uint32_t TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES;
 
-typedef struct {
+typedef struct
+{
     uint32_t num_rows;
-    void* pages[TABLE_MAX_PAGES];
+    void *pages[TABLE_MAX_PAGES];
 } Table;
 
+void *row_slot(Table *table, uint32_t row_num)
+{
+    uint32_t page_num = row_num / ROWS_PER_PAGE;
+    void* page = table -> pages[page_num];
+    if(page == NULL) {
+        page = table -> pages[page_num] = malloc(PAGE_SIZE);
+    }
+    uint32_t row_offset = row_num % ROWS_PER_PAGE;
+    uint32_t byte_offset = row_offset * ROW_SIZE;
+    return page + byte_offset;
+}
 MetaCommandResult do_meta_command(InputBuffer *input_buffer)
 {
     if (strcmp(input_buffer->buffer, ".exit") == 0)
@@ -79,9 +99,9 @@ PrepareResult prepare_statement(InputBuffer *input_buffer, Statement *statement)
         statement->type = STATEMENT_INSERT;
         int args_assigned = sscanf(
             input_buffer->buffer, "insert %d %s %s", &(statement->row_to_insert.id),
-            statement->row_to_insert.username, statement->row_to_insert.email
-        );
-        if(args_assigned < 3) {
+            statement->row_to_insert.username, statement->row_to_insert.email);
+        if (args_assigned < 3)
+        {
             return PREPARE_SYNTAX_ERROR;
         }
         return PREPARE_SUCCESS;
