1f103ac658afe1946042e255c03b948e3b93c1c8
Jose Chu <jchudb93@gmail.com>
2019-10-20 17:10:44 -0500

    size

--- commands.h
+++ commands.h
@@ -1,7 +1,11 @@
 
 #include "buffer.h"
+#include <stdint.h>
+#define COLUMN_USERNAME_SIZE 32
+#define COLUMN_EMAIL_SIZE 255
 #ifndef COMMANDS
 #define COMMANDS
+
 
 typedef enum
 {
@@ -20,10 +24,25 @@ typedef enum
     PREPARE_UNRECOGNIZED_STATEMENT
 } PrepareResult;
 
+typedef struct {
+    uint32_t id;
+    char username[COLUMN_USERNAME_SIZE];
+    char email[COLUMN_EMAIL_SIZE];
+} Row;
 typedef struct
 {
     StatementType type;
+    Row row_to_insert;
 } Statement;
+
+#define size_of_attribute(Struct, Attribute) sizeof(((Struct*)0)->Attribute)
+const uint32_t ID_SIZE = size_of_attribute(Row, id);
+const uint32_t USERNAME_SIZE = size_of_attribute(Row, username);
+const uint32_t EMAIL_SIZE = size_of_attribute(Row, email);
+const uint32_t ID_OFFSET = 0;
+const uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
+const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
+const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
 
 MetaCommandResult do_meta_command(InputBuffer *input_buffer)
 {
@@ -42,6 +61,13 @@ PrepareResult prepare_statement(InputBuffer *input_buffer, Statement *statement)
     if (strncmp(input_buffer->buffer, "insert", 6) == 0)
     {
         statement->type = STATEMENT_INSERT;
+        int args_assigned = sscanf(
+            input_buffer->buffer, "insert %d %s %s", &(statement->row_to_insert.id),
+            statement->row_to_insert.username, statement->row_to_insert.email
+        );
+        if(args_assigned < 3) {
+            return PREPARE_SYNTAX_ERROR;
+        }
         return PREPARE_SUCCESS;
     }
     if (strcmp(input_buffer->buffer, "select") == 0)
